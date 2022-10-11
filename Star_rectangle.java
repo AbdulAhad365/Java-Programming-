fe0f01808385824ac69a16a769956a31dbebd4f4
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2022-10-11 16:30:02 -0700

    Printing incrementing the tringle from least to most by for "*"

---
+++ Star_rectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Scratch {
+    public static void main(String[] args) {
+        System.out.println("Program to print reverse of 3 digit");
+        int num = 5;
+        for (int i = 0; i < 4; i++) {
+            for (int j=0;j<=i;j++){
+                System.out.print("*");  // This conditions is check again by incrementing the main condition
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+}
