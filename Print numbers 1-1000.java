718f935316078b6460a2d69295d23c916f111fb5
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2022-10-10 18:06:19 -0700

    For printing numbers from 1 to 1000 by while

---
+++ Print numbers 1-1000.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Name_by_String {
+// Program TO PRINT NUMBERS 1 TO 1000
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=1;
+        while (num<=1000){    //while is a loop
+            System.out.println(num);    // there are three kinds of loop:
+            /**
+             * 1)   FOR LOOP
+             * 2)    WHILE LOOP
+             * 3)     DO WHILE LOOP
+             * */
+            num++;
+        }
+    }
+}
