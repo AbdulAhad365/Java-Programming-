0b671347f4c35613eb5430f2e5e03e705fb7a039
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2022-10-22 10:31:04 -0700

    How you can compare two String and its use in program

---
+++ Compare_two_String.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Declare_Empty_Array {
+    public static void main(String args[]) {
+        System.out.println("ali"=="ali");
+        Scanner s=new Scanner(System.in);
+        String x="ali";
+        String y="ali";
+        String n1=s.next();
+        String n2=s.next();
+        if (n1.equals(n1)==n2.equals(n2)){
+            System.out.println("good");
+        }
+        }
+    }
