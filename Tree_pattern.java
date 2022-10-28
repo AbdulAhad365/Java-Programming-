28c88f3cd14bb2c58c52f53d9e3064b46e7e526f
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2022-10-28 08:18:52 -0700

    Tree pattern program

---
+++ Tree_pattern.java
@@ -0,0 +1,29 @@
+class LabeledForExample {
+    public static void main(String[] args) {
+        //Using Label for outer and for loop
+        int rows=4;
+        for(int i=0; 4>=i;i++){
+            // Spacing used in the nested for
+            for (int s=1;s<=rows;s++) {
+                System.out.print(" ");
+            }
+                // Now for the printing of traingle
+            for (int j=1;i>=j;j++){
+                System.out.print("* ");
+                }
+                System.out.println();
+                rows--;
+            }
+
+        for (int i=0;i<=3;i++){
+            System.out.print("  ");
+            for (int s=1;2>=s;s++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        }
+        }
+
+
